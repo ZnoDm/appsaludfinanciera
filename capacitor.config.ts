069a35c30288c7f618c1c1e13b3ea51883c7f5bd
Nelson ANgeles <n.angelesp@hotmail.com>
2023-10-02 16:57:59 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'saludfinanciera-app',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
